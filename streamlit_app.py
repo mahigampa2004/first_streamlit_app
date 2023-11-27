1a9526a81c37dcba33b55c04ee68dcf055e7987b
mahigampa2004 <152166018+mahigampa2004@users.noreply.github.com>
2023-11-27 16:13:21 +0530

    Update streamlit_app.py
    Updated text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blackberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+
