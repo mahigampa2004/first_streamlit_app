ff354f481ac9e773eee81314732492065c7fa174
mahigampa2004 <152166018+mahigampa2004@users.noreply.github.com>
2023-11-28 11:51:27 +0530

    Update streamlit_app.py
    Changed the standard.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner ==>')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blackberry Oatmeal')
@@ -8,7 +11,5 @@ streamlit.text('Kale,Spinach & Rocket Smoothie')
 
 streamlit.header('Build your own fruit smoothie')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
