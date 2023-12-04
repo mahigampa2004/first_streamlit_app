af2b5cd0d425cd066dab316d8444c3a791809230
mahigampa2004 <152166018+mahigampa2004@users.noreply.github.com>
2023-12-04 12:52:11 +0530

    Update streamlit_app.py
    ..

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 # Display the table on the page.
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("View our Fruit List - Add your Favorites!")
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -52,6 +52,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
